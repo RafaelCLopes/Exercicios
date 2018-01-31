f89f8e3daa4385489792ab189ff753bea7f4e9af
RAFAEL <rclrafael@hotmail.com>
2018-01-31 14:24:15 -0200

    primeiro checkin

---
+++ CaixaEletronico/CaixaEletronico/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace CaixaEletronico
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.lblSaque = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(91, 80);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 49);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Saque";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtValor
+            // 
+            this.txtValor.Location = new System.Drawing.Point(91, 42);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(100, 20);
+            this.txtValor.TabIndex = 1;
+            // 
+            // lblSaque
+            // 
+            this.lblSaque.AutoSize = true;
+            this.lblSaque.Location = new System.Drawing.Point(124, 26);
+            this.lblSaque.Name = "lblSaque";
+            this.lblSaque.Size = new System.Drawing.Size(35, 13);
+            this.lblSaque.TabIndex = 2;
+            this.lblSaque.Text = "Sacar";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lblSaque);
+            this.Controls.Add(this.txtValor);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.Label lblSaque;
+    }
+}
+
---
+++ CaixaEletronico/CaixaEletronico/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CaixaEletronico
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            double saldo = 100;
+            double valor = Double.Parse(txtValor.Text);
+            if (valor <= saldo)
+            {
+                saldo = saldo - valor;
+                MessageBox.Show("Saque realizado com Sucesso");
+
+            }
+            else
+                MessageBox.Show("Saldo Insuficiente");
+               }
+    }
+}
